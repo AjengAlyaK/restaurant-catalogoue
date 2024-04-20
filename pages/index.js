9636e9e633c792953f2917eac5bca332c5e4a988
AjengAlyaK <ajenga990@gmail.com>
2024-04-20 15:34:59 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import RestaurantList from '../components/RestaurantsList';
+
+export default function Home({ restaurants }) {
+  return (
+    <div className='container'>
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch('https://restaurant-api.dicoding.dev/list');
+  const { restaurants } = await response.json();
+
+  return {
+    props: {
+      restaurants,
+    },
+  }
+}
